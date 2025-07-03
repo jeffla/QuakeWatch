f4e54dfabfc03dcf05f58402c3d5431a7f56a02c
Jeff Lacey <jeff@jclacey.com>
2025-07-03 16:35:34 -0500

    Initial Commit

---
+++ QuakeWatch/QuakeWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuakeWatchApp.swift
+//  QuakeWatch
+//
+//  Created by Jeff Lacey on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct QuakeWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuakeWatchTests/QuakeWatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuakeWatchTests.swift
+//  QuakeWatchTests
+//
+//  Created by Jeff Lacey on 7/3/25.
+//
+
+import Testing
+@testable import QuakeWatch
+
+struct QuakeWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
